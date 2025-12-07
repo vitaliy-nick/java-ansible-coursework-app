1ecd8ca74bdafe95b3cf9e588a92d1213250f5a2
vitaliy-nick <pavlus.vit@gmail.com>
2025-12-07 15:28:42 +0200

    feat: Add main Java application file and fix Maven build

---
+++ src/main/java/com/coursework/javaapp/JavaAppApplication.java
@@ -0,0 +1,23 @@
+// src/main/java/com/coursework/javaapp/JavaAppApplication.java
+
+package com.coursework.javaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JavaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAppApplication.class, args);
+    }
+
+    // Простий REST-контролер для тестування (як у Розділі 4)
+    @GetMapping("/api/status")
+    public String getStatus() {
+        return "Java Application is running! Version 0.0.1";
+    }
+}
